098c3528a158cd34b2fc19da49e552d89a38c559
root <root@debian.DEbian>
2021-12-19 07:05:59 -0500

    connection refused maria_db a debug

--- srcs/requirements/mariadb/conf/start.sh
+++ srcs/requirements/mariadb/conf/start.sh
@@ -10,9 +10,16 @@
 
 /usr/bin/mysqld_safe & while ! nc -vz localhost 3306; do sleep 1; done
 
-/usr/bin/mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci"
-/usr/bin/mysql -uroot -e "GRANT ALL ON $MYSQL_DATABASE .* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-/usr/bin/mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+# /usr/bin/mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci"
+# /usr/bin/mysql -uroot -e "GRANT ALL ON $MYSQL_DATABASE .* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+# /usr/bin/mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+
+# service mysql start
+echo "CREATE DATABASE $MYSQL_DATABASE;" | mysql -u root
+echo "CREATE USER '$MYSQL_USER'@'%';" | mysql -u root
+echo "SET password FOR '$MYSQL_USER'@'%' = password('$MYSQL_PASSWORD');" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -u root
+echo "FLUSH PRIVILEGES;" | mysql -u root
 
 mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
 
