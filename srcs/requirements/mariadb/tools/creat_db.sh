479b0826c65d40652084926858179736a940343e
Sacha Baranes <user@iMac-de-user-5.local>
2021-12-24 08:21:22 +0100

    test

--- srcs/requirements/mariadb/tools/creat_db.sh
+++ srcs/requirements/mariadb/tools/creat_db.sh
@@ -1,11 +1,38 @@
 if [ ! -d "/tmp/ok" ]; then
-	mysql_install_db
+	mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 	service mysql start
-	echo "UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE User = 'root';" | mysql -u root
-	echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('root');" | mysql -u root
-	echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci" | mysql -u root -proot
-	echo "GRANT ALL ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -u root -proot
-	echo "FLUSH PRIVILEGES;" | mysql -u root -proot
+
+	SECURE_MYSQL=$(expect -c "
+set timeout 2
+spawn mysql_secure_installation
+expect \"Enter current password for root (enter for none):\"
+send \"\r\"
+expect \"Change the root password?\"
+send \"y\r\"
+expect \"New password:\"
+send \"$MYSQL_ROOT_PASSWORD\r\"
+expect \"Re-enter new password:\"
+send \"$MYSQL_ROOT_PASSWORD\r\"
+expect \"Remove anonymous users?\"
+send \"y\r\"
+expect \"Disallow root login remotely?\"
+send \"n\r\"
+expect \"Remove test database and access to it?\"
+send \"y\r\"
+expect \"Reload privilege tables now?\"
+send \"y\r\"
+expect eof
+")
+	echo "$SECURE_MYSQL"
+
+	echo
+
+	readonly Q1="CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci"
+	readonly Q2="GRANT ALL ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+	readonly Q3="CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY 'MYSQL_PASSWORD';"
+	readonly Q4="FLUSH PRIVILEGES;"
+	readonly SQL="${Q1}${Q2}${Q3}${Q4}"
+	$MYSQL -uroot -p$MYSQL_ROOT_PASSWORD -e "$SQL"
 	mkdir /tmp/ok
 
 fi
--- srcs/requirements/mariadb/tools/create_env.sh
+++
@@ -1,4 +0,0 @@
-sed -i 's/MYSQL_DATABASE/'${MYSQL_DATABASE}'/g' creat_db.sh
-sed -i 's/MYSQL_USER/'${MYSQL_USER}'/g' creat_db.sh
-sed -i 's/MYSQL_PASSWORD/'${MYSQL_PASSWORD}'/g' creat_db.sh
-sed -i 's/MYSQL_ROOT_PASSWORD/'${MYSQL_ROOT_PASSWORD}'/g' creat_db.sh
