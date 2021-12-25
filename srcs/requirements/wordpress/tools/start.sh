333e36479af26c5f2f7696d2fcab75e7d05f963c
sacha_vm <sachabaranes0@gmail.com>
2021-12-25 10:31:02 -0500

    need continue

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -7,11 +7,9 @@ sed -i 's/MYSQL_USER/'${MYSQL_USER}'/g' wp-config.php
 sed -i 's/MYSQL_PASSWORD/'${MYSQL_PASSWORD}'/g' wp-config.php
 sed -i 's/MYSQL_HOST/'${MYSQL_HOST}'/g' wp-config.php
 cp wp-config.php var/www/wordpress
-cd var/www/wordpress && \
-wp-cli core download --allow-root && \
-wp-cli core install --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${MYSQL_USER} --admin_password=${MYSQL_PASSWORD} --admin_email=${WP_MAIL} --allow-root && \
-# wp user create ${USER_NAME} ${EMAIL_ADDRESS} --role=author --user_pass=${USER_PASSWORD} --allow-root && \
-cd ../../..
+wp-cli core download --path=/var/www/wordpress --allow-root
+wp-cli core install --path=/var/www/wordpress --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${MYSQL_USER} --admin_password=${MYSQL_PASSWORD} --admin_email=${WP_MAIL} --allow-root
+# wp-cli user create --path=/var/www/wordpress ${USER_NAME} ${EMAIL_ADDRESS} --role=author --user_pass=${USER_PASSWORD} --allow-root
 cp www.conf etc/php/7.3/fpm/pool.d
 service php7.3-fpm start
 service php7.3-fpm stop
